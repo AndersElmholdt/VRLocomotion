7cac17cbf29cb7b686a0d116c2227a78a22f360f
Anders Elmholdt <anders@elmholdt.dk>
2017-03-11 21:10:49 +0100

    Updated LocSaver
    Updated LocSaver to include rotation and velocity

--- Source/VRLocomotion/LocSaver.cpp
+++ Source/VRLocomotion/LocSaver.cpp
@@ -12,20 +12,27 @@ ULocSaver::ULocSaver()
 	PrimaryComponentTick.bCanEverTick = false;
 
 	// Default values
-	RecordRate = 60;
+	RecordRate = 90;
 	Record = true;
 	AllowOverwriting = false;
 	CurrentTimeIndex = 0;
 	PlaybackRateScale = 1.0f;
-	PlaybackRate = 60;
+	PlaybackRate = 90;
 	PositionOffset = FVector(0.0, 0.0, 0.0);
+	RotationOffset = FVector(0.0, 0.0, 0.0);
+	ApplyRotation = true;
+	Velocity = FVector(0.0, 0.0, 0.0);
+	VelocityInfluencedByPlayback = false;
 }
 
 // Called when actor is destroyed, or the game ends
-ULocSaver::~ULocSaver() {
-	// Write the location data when game is closed
+ULocSaver::~ULocSaver()
+{
+	// Write the location data if we are recording
 	if (Record)
+	{
 		WriteToFile();
+	}
 }
 
 
@@ -34,14 +41,20 @@ void ULocSaver::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// If recording, call the function RecordCurrentLocation at the specified record rate
-	if (Record) {
+	// Check if we should record or playback a file
+	if (Record)
+	{
+		// Sets a timer for RecordCurrentLocation at the specified record rate
 		GetOwner()->GetWorldTimerManager().SetTimer(LocationSaverHandle, this, &ULocSaver::RecordCurrentLocation, 1 / float(RecordRate), true);
 	}
-
-	// Otherwise, read the file and call SetCurrentLocation instead
-	else {
+	else
+	{
 		ReadFromFile();
+
+		// Convert roation offset from euler to quaternion
+		RotationOffsetQuat = RotationOffset.Rotation().Quaternion();
+
+		// Sets a timer for SetCurrentLocation at the specified playbackrate
 		GetOwner()->GetWorldTimerManager().SetTimer(LocationSaverHandle, this, &ULocSaver::SetCurrentLocation, 1 / (PlaybackRate * PlaybackRateScale), true);
 	}
 }
@@ -55,32 +68,41 @@ void ULocSaver::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 
 
 // Writes the location data to a file
-void ULocSaver::WriteToFile() {
+void ULocSaver::WriteToFile() const
+{
 	// Check if file name is not null
-	if (!FileName.Equals("")) {
-		FString Directory = FPaths::GameDir() + "LocationData/";
+	if (!FileName.Equals("") && Locations.Num() > 0)
+	{
+		const FString Directory = FPaths::GameDir() + "LocationData/";
 		IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
 
 		// Verify or create the directory
-		if (!PlatformFile.DirectoryExists(*Directory)) {
+		if (!PlatformFile.DirectoryExists(*Directory))
+		{
 			PlatformFile.CreateDirectory(*Directory);
 
 			// Failed to create the directory
-			if (!PlatformFile.DirectoryExists(*Directory)) {
+			if (!PlatformFile.DirectoryExists(*Directory))
+			{
 				return;
 			}
 		}
 
 		// Convert array storing location data to a string
 		FString SaveData = FString::FromInt(RecordRate) + "\n";
-		for (auto& vec : Locations) {
-			SaveData.Append(vec.ToString() + "\n");
+		for (int32 i = 0; i != Locations.Num(); ++i)
+		{
+			SaveData.Append(Locations[i].ToString() + "\n");
+			SaveData.Append(Rotations[i].ToString() + "\n");
+			SaveData.Append(VelocityArray[i].ToString() + "\n");
 		}
 
 		// Save the string to file
-		FString AbsoluteFilePath = Directory + FileName + ".loc";
-		if (PlatformFile.CreateDirectoryTree(*Directory)) {
-			if (AllowOverwriting || !PlatformFile.FileExists(*AbsoluteFilePath)) {
+		const FString AbsoluteFilePath = Directory + FileName + ".loc";
+		if (PlatformFile.CreateDirectoryTree(*Directory))
+		{
+			if (AllowOverwriting || !PlatformFile.FileExists(*AbsoluteFilePath))
+			{
 				FFileHelper::SaveStringToFile(SaveData, *AbsoluteFilePath);
 			}
 		}
@@ -89,62 +111,108 @@ void ULocSaver::WriteToFile() {
 
 
 // Reads the location data from a file
-void ULocSaver::ReadFromFile() {
+void ULocSaver::ReadFromFile()
+{
 	// Check if file name is not null
-	if (!FileName.Equals("")) {
-		FString Directory = FPaths::GameDir() + "LocationData/";
+	if (!FileName.Equals(""))
+	{
+		const FString Directory = FPaths::GameDir() + "LocationData/";
 		IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
 
 		// Verify the directory
-		if (!PlatformFile.DirectoryExists(*Directory)) {
+		if (!PlatformFile.DirectoryExists(*Directory))
+		{
+			// Print error message
 			if (GEngine)
+			{
 				GEngine->AddOnScreenDebugMessage(-1, 15, FColor::Red, TEXT("LocSaver Error: Directory doesn't exist"));
+			}
 			return;
 		}
 
 		// Load data to a string
 		FString LoadData;
-		FString AbsoluteFilePath = Directory + FileName + ".loc";
-		if (PlatformFile.CreateDirectoryTree(*Directory)) {
-			if (PlatformFile.FileExists(*AbsoluteFilePath)) {
+		const FString AbsoluteFilePath = Directory + FileName + ".loc";
+		if (PlatformFile.CreateDirectoryTree(*Directory))
+		{
+			if (PlatformFile.FileExists(*AbsoluteFilePath))
+			{
 				FFileHelper::LoadFileToString(LoadData, *AbsoluteFilePath);
 			}
-			else {
+			else
+			{
 				return;
 			}
 		}
 
 		// Use this to split the string by certain characters
-		const TCHAR * Delims[] = { TEXT("X"), TEXT("Y"), TEXT("Z"), TEXT(" "), TEXT("\n"), TEXT("=") };
+		const TCHAR * Delims[] = { TEXT("X"), TEXT("Y"), TEXT("Z"), TEXT(" "), TEXT("\n"), TEXT("="), TEXT("W") };
 
 		// Split string into an array of strings
 		TArray<FString> LoadDataArray;
-		LoadData.ParseIntoArray(LoadDataArray, Delims, 6, true);
+		LoadData.ParseIntoArray(LoadDataArray, Delims, 7, true);
 
-		// Convert array of strings to array of FVectors
+		// Convert array of strings to array of FVectors/FQuats
 		PlaybackRate = FCString::Atoi(*LoadDataArray[0]);
-		for (int i = 1; i < LoadDataArray.Num(); i = i + 3) {
+		for (int i = 1; i < LoadDataArray.Num(); i = i + 10)
+		{
 			Locations.Add(FVector(FCString::Atof(*LoadDataArray[i]), FCString::Atof(*LoadDataArray[i + 1]), FCString::Atof(*LoadDataArray[i + 2])));
+			Rotations.Add(FQuat(FCString::Atof(*LoadDataArray[i + 3]), FCString::Atof(*LoadDataArray[i + 4]), FCString::Atof(*LoadDataArray[i + 5]), FCString::Atof(*LoadDataArray[i + 6])));
+			VelocityArray.Add(FVector(FCString::Atof(*LoadDataArray[i + 7]), FCString::Atof(*LoadDataArray[i + 8]), FCString::Atof(*LoadDataArray[i + 9])));
 		}
 	}
 }
 
 
 // Adds the current location to the Locations array
-void ULocSaver::RecordCurrentLocation() {
+void ULocSaver::RecordCurrentLocation()
+{
 	Locations.Add(GetOwner()->GetTransform().GetTranslation());
+	Rotations.Add(GetOwner()->GetTransform().GetRotation());
+	Velocity = GetOwner()->GetVelocity();
+	VelocityArray.Add(Velocity);
 }
 
 
 // Sets the actors current location based on the Locations array
-void ULocSaver::SetCurrentLocation() {
+void ULocSaver::SetCurrentLocation()
+{
 	// If the script has iterated over the last index of the array, stop the timer and exit
-	if (CurrentTimeIndex + 1 >= Locations.Num()) {
+	if (CurrentTimeIndex + 1 >= Locations.Num())
+	{
 		GetOwner()->GetWorldTimerManager().ClearTimer(LocationSaverHandle);
+		Velocity = FVector(0, 0, 0);
 		return;
 	}
 
-	// Updates the location
-	GetOwner()->SetActorLocation(Locations[++CurrentTimeIndex] + PositionOffset, false);
+	// Update the velocity variable
+	if (VelocityInfluencedByPlayback)
+	{
+		Velocity = VelocityArray[CurrentTimeIndex] * PlaybackRateScale;
+	}
+	else
+	{
+		Velocity = VelocityArray[CurrentTimeIndex];
+	}
+
+	// Updates the location - PositionOffset is added to the location from the file
+	GetOwner()->SetActorLocation(Locations[CurrentTimeIndex] + PositionOffset, false);
+
+	if (ApplyRotation)
+	{
+		// Updates the rotation - RotationOffsetQuat is multiplied with the roation value as that is how you add quaternions
+		GetOwner()->SetActorRotation(RotationOffsetQuat * Rotations[CurrentTimeIndex]);
+	}
+
+	CurrentTimeIndex++;
 }
 
+
+/**
+* Get the current velocity of the object
+* @return The current velocity
+*/
+FVector ULocSaver::GetVelocity() const
+{
+	return Velocity;
+}
--- Source/VRLocomotion/LocSaver.h
+++ Source/VRLocomotion/LocSaver.h
@@ -6,7 +6,7 @@
 #include "LocSaver.generated.h"
 
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class VRLOCOMOTION_API ULocSaver : public UActorComponent
 {
 	GENERATED_BODY()
@@ -21,7 +21,7 @@ public:
 		bool Record;
 
 	// How many times the position is saved per second
-	UPROPERTY(EditAnywhere, meta = (ClampMin = "1", ClampMax = "120", EditCondition = "Record"))
+	UPROPERTY(EditAnywhere, meta = (ClampMin = "1", ClampMax = "180", EditCondition = "Record"))
 		int RecordRate;
 
 	// Allows the script to overwrite files
@@ -32,9 +32,32 @@ public:
 	UPROPERTY(EditAnywhere, meta = (ClampMin = "0.1", ClampMax = "2.0", EditCondition = "!Record"))
 		float PlaybackRateScale;
 
+	// Whether or not the Playback Rate Scale influences the velocity
+	UPROPERTY(EditAnywhere, meta = (EditCondition = "!Record"))
+		bool VelocityInfluencedByPlayback;
+
 	// Offsets the position from the recorded data
 	UPROPERTY(EditAnywhere, meta = (EditCondition = "!Record"))
 		FVector PositionOffset;
+
+	// Whether or not the recorded rotation should be applied
+	UPROPERTY(EditAnywhere, meta = (EditCondition = "!Record"))
+		bool ApplyRotation;
+
+	// Offsets the rotation from the recorded data
+	UPROPERTY(EditAnywhere, meta = (EditCondition = "!Record"))
+		FVector RotationOffset;
+
+	/**
+	* Get the current velocity of the object
+	* @return The current velocity
+	*/
+	UFUNCTION(BlueprintCallable)
+		FVector GetVelocity() const;
+
+private:
+	// Converted rotation offset
+	FQuat RotationOffsetQuat;
 
 	// The playback rate that is loaded from the file
 	int PlaybackRate;
@@ -42,14 +65,23 @@ public:
 	// Location array storing the location information
 	TArray<FVector> Locations;
 
+	// Rotation array storing the rotation information
+	TArray<FQuat> Rotations;
+
 	// The timer handle for how often to record/set locations
 	FTimerHandle LocationSaverHandle;
 
 	// Determines the current index in the locations array when playing back
 	int CurrentTimeIndex;
 
+	// The velocity of the object
+	FVector Velocity;
+
+	// The velocity of the object to save/read from the file
+	TArray<FVector> VelocityArray;
+
 	// Writes the location data to a file
-	void WriteToFile();
+	void WriteToFile() const;
 
 	// Reads the location data from a file
 	void ReadFromFile();
